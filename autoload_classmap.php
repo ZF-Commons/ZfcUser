22bca02bd468c19ef4134eec2074f28502cb8072
Evan Coury <me@evancoury.com>
2011-11-04 20:18:53 -0700

    Update classmap

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,2 +1,13 @@
 <?php
-return array();
+return array(
+    'EdpUser\Mapper\UserDoctrine'       => __DIR__ . '/src/EdpUser/Mapper/UserDoctrine.php',
+    'EdpUser\Mapper\UserInterface'      => __DIR__ . '/src/EdpUser/Mapper/UserInterface.php',
+    'EdpUser\Mapper\UserZendDb'         => __DIR__ . '/src/EdpUser/Mapper/UserZendDb.php',
+    'EdpUser\Form\Register'             => __DIR__ . '/src/EdpUser/Form/Register.php',
+    'EdpUser\Form\Base'                 => __DIR__ . '/src/EdpUser/Form/Base.php',
+    'EdpUser\Form\Login'                => __DIR__ . '/src/EdpUser/Form/Login.php',
+    'EdpUser\Model\User'                => __DIR__ . '/src/EdpUser/Model/User.php',
+    'EdpUser\Service\User'              => __DIR__ . '/src/EdpUser/Service/User.php',
+    'EdpUser\Controller\UserController' => __DIR__ . '/src/EdpUser/Controller/UserController.php',
+    'EdpUser\Module'                    => __DIR__ . '/Module.php',
+);
