b13df5b91781e5acf672396ea4d6b59782cb0b85
Daniel Rossi <danielr@i.local>
2012-01-19 20:50:52 +1100

    Added registration events

--- src/ZfcUser/Controller/UserController.php
+++ src/ZfcUser/Controller/UserController.php
@@ -125,6 +125,7 @@ class UserController extends ActionController
         if ($request->isPost()) {
             if (false === $form->isValid($request->post()->toArray())) {
                 $this->flashMessenger()->setNamespace('zfcuser-register-form')->addMessage($request->post()->toArray());
+                $this->events()->trigger('register-validate-error', $this, array('form' => $form));
                 return $this->redirect()->toRoute('zfcuser/register');
             } else {
                 $this->getUserService()->createFromForm($form);
