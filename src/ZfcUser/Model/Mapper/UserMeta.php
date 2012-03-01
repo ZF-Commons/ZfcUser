fdbad956ce5c4466e26b34914be47938185478b4
Tom Anderson <tom.h.anderson@gmail.com>
2012-03-01 10:51:02 -0800

    Removed type cast to allow for doctrine custom entities

--- src/ZfcUser/Model/Mapper/UserMeta.php
+++ src/ZfcUser/Model/Mapper/UserMeta.php
@@ -2,13 +2,11 @@
 
 namespace ZfcUser\Model\Mapper;
 
-use ZfcUser\Model\UserMeta as UserMetaModel;
-
 interface UserMeta
 {
-    public function add(UserMetaModel $userMeta);
+    public function add($userMeta);
 
-    public function update(UserMetaModel $userMeta);
+    public function update($userMeta);
 
     public function get($userId, $metaKey);
 }
