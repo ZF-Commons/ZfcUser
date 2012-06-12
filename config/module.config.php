0d6bbdf9d221186f60df1166aa09728de713d525
Adam Lundrigan <adamlundrigan@cdli.ca>
2012-06-12 10:00:38 -0230

    Add comment to module.config.php explaining the auth_identity_fields override

--- config/module.config.php
+++ config/module.config.php
@@ -4,6 +4,10 @@ return array(
         'user_model_class'          => 'ZfcUser\Model\User',
         'usermeta_model_class'      => 'ZfcUser\Model\UserMeta',
         'enable_registration'       => true,
+        //NOTE: Please override the setting below via your zfcuser.global.php file
+        //      Uncommenting the line below will break any overrides in later config files
+        //      due to the way config file merging works with array values
+        //'auth_identity_fields'      => array( 'email' ),
         'enable_username'           => false,
         'enable_display_name'       => false,
         'require_activation'        => false,
