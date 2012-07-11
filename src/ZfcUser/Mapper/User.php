977cd4a4ff1bb2e78a0d86db967b04ca8d695e5e
Nicholas Calugar <njcalugar@gmail.com>
2012-07-11 11:43:48 -0700

    When inserting an identity, set the ID from the generated value

--- src/ZfcUser/Mapper/User.php
+++ src/ZfcUser/Mapper/User.php
@@ -43,6 +43,13 @@ class User extends AbstractDbMapper implements UserInterface
         return $entity;
     }
 
+    public function insert($entity, $tableName = null, HydratorInterface $hydrator = null)
+    {
+        $result = parent::insert($entity, $tableName, $hydrator);
+        $entity->setId($result->getGeneratedValue());
+        return $result;
+    }
+
     public function update($entity, $where = null, $tableName = null, HydratorInterface $hydrator = null)
     {
         if (!$where) {
