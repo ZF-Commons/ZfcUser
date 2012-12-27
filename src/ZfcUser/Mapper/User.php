929267a45130bfecc9eaef3c36e162395c3a49d1
Thoai Nguyen <me@thoaionline.com>
2012-12-27 23:35:26 +1100

    make use of custom UserHydrator

--- src/ZfcUser/Mapper/User.php
+++ src/ZfcUser/Mapper/User.php
@@ -5,6 +5,7 @@ namespace ZfcUser\Mapper;
 use ZfcBase\Mapper\AbstractDbMapper;
 use ZfcUser\Entity\UserInterface as UserEntityInterface;
 use Zend\Stdlib\Hydrator\HydratorInterface;
+use ZfcBase\Mapper\UserHydrator;
 
 class User extends AbstractDbMapper implements UserInterface
 {
@@ -39,9 +40,19 @@ class User extends AbstractDbMapper implements UserInterface
         $this->getEventManager()->trigger('find', $this, array('entity' => $entity));
         return $entity;
     }
+    
+    protected $hydrator;
+    
+    public function getHydrator(){
+    	if (!isset($this->hydrator)){
+    		$this->hydrator=new UserHydrator();    		
+    	}
+    	return $this->hydrator;
+    }
 
     public function insert($entity, $tableName = null, HydratorInterface $hydrator = null)
     {
+    	if ($hydrator==null) $hydrator=$this->getHydrator();
         $result = parent::insert($entity, $tableName, $hydrator);
         $entity->setId($result->getGeneratedValue());
         return $result;
@@ -49,10 +60,14 @@ class User extends AbstractDbMapper implements UserInterface
 
     public function update($entity, $where = null, $tableName = null, HydratorInterface $hydrator = null)
     {
+    	if ($hydrator==null) $hydrator=$this->getHydrator();
+    	
         if (!$where) {
             $where = 'user_id = ' . $entity->getId();
         }
 
         return parent::update($entity, $where, $tableName, $hydrator);
     }
+    
+
 }
