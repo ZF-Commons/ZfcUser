c8c357acbea84f6c3d76e9c08c22e62473854de1
Emmanuel Belair <contact@ebelair.fr>
2013-02-12 10:11:52 +0100

    fr_FR.po file génération and translation

---
+++ src/ZfcUser/language/msgIds.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Ids for poedit
+ * @method Z
+ */
+translate('Username');
+translate('Email');
+translate('New Email');
+translate('Verify New Email');
+translate('Display Name');
+translate('Password');
+translate('Password Verify');
+translate('Current Password');
+translate('Verify New Password');
+translate('New Password');
+translate('Please type the following text');
+translate('Submit');
+translate('Sign In');
+translate('Register');
