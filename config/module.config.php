cb516cb074023230e3299994eafd79b2d1f8a553
Eugene <eugene@galenko.com>
2013-07-22 13:09:07 +0300

    Russian translation

--- config/module.config.php
+++ config/module.config.php
@@ -93,4 +93,14 @@ return array(
             ),
         ),
     ),
+
+    'translator' => array(
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
 );
