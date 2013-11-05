9d5ee267024d96ccde3e33af674d78aebb419355
George Cooksey <george.cooksey@bytesofknowledge.com>
2013-11-05 16:05:15 -0600

    Remove setters from UserInterface
    Setters are potentially unnecessary and may vary per implementation due to PHP's limitations on type-casting and method signatures.

--- src/ZfcUser/Entity/UserInterface.php
+++ src/ZfcUser/Entity/UserInterface.php
@@ -4,20 +4,13 @@ namespace ZfcUser\Entity;
 
 interface UserInterface
 {
+
     /**
      * Get id.
      *
      * @return int
      */
     public function getId();
-
-    /**
-     * Set id.
-     *
-     * @param int $id
-     * @return UserInterface
-     */
-    public function setId($id);
 
     /**
      * Get username.
@@ -27,27 +20,11 @@ interface UserInterface
     public function getUsername();
 
     /**
-     * Set username.
-     *
-     * @param string $username
-     * @return UserInterface
-     */
-    public function setUsername($username);
-
-    /**
      * Get email.
      *
      * @return string
      */
     public function getEmail();
-
-    /**
-     * Set email.
-     *
-     * @param string $email
-     * @return UserInterface
-     */
-    public function setEmail($email);
 
     /**
      * Get displayName.
@@ -57,27 +34,11 @@ interface UserInterface
     public function getDisplayName();
 
     /**
-     * Set displayName.
-     *
-     * @param string $displayName
-     * @return UserInterface
-     */
-    public function setDisplayName($displayName);
-
-    /**
      * Get password.
      *
      * @return string password
      */
     public function getPassword();
-
-    /**
-     * Set password.
-     *
-     * @param string $password
-     * @return UserInterface
-     */
-    public function setPassword($password);
 
     /**
      * Get state.
@@ -86,11 +47,4 @@ interface UserInterface
      */
     public function getState();
 
-    /**
-     * Set state.
-     *
-     * @param int $state
-     * @return UserInterface
-     */
-    public function setState($state);
 }
