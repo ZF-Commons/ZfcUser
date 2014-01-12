7e5365fa4a877db7667af3e98a169d82a5e3308e
Daniel Strøm <danielss89@gmail.com>
2014-01-12 21:54:37 +0100

    Supress phpcs warnings

--- tests/ZfcUserTest/View/Helper/ZfcUserLoginWidgetTest.php
+++ tests/ZfcUserTest/View/Helper/ZfcUserLoginWidgetTest.php
@@ -6,7 +6,19 @@ use ZfcUser\View\Helper\ZfcUserLoginWidget as ViewHelper;
 
 class ZfcUserLoginWidget extends \PHPUnit_Framework_TestCase
 {
-    public function testInvoke()
+    protected $helper;
+
+    public function setUp()
+    {
+        $this->helper = new \ZfcUser\View\Helper\ZfcUserLoginWidget;
+    }
+
+    public function testInvokeWithRender()
+    {
+        $this->helper->__invoke(array());
+    }
+
+    public function testInvokeWithoutRender()
     {
         $this->markTestIncomplete(
             'This test has not been implemented yet.'
