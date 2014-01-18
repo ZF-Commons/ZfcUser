69c4355064f366337fd7dc7ab0468185c3c70931
Daniel Strøm <danielss89@gmail.com>
2014-01-18 19:16:50 +0100

    Added tests for RecordExists validator

--- tests/ZfcUserTest/Validator/RecordExistsTest.php
+++ tests/ZfcUserTest/Validator/RecordExistsTest.php
@@ -6,10 +6,45 @@ use ZfcUser\Validator\RecordExists as Validator;
 
 class RecordExistsTest extends \PHPUnit_Framework_TestCase
 {
+    protected $validator;
+
+    protected $mapper;
+
+    public function setUp()
+    {
+        $options = array('key' => 'username');
+        $validator = new Validator($options);
+        $this->validator = $validator;
+
+        $mapper = $this->getMock('ZfcUser\Mapper\UserInterface');
+        $this->mapper = $mapper;
+
+        $validator->setMapper($mapper);
+    }
+
     public function testIsValid()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $this->mapper->expects($this->once())
+                     ->method('findByUsername')
+                     ->with('zfcUser')
+                     ->will($this->returnValue('zfcUser'));
+
+        $result = $this->validator->isValid('zfcUser');
+        $this->assertTrue($result);
+    }
+
+    public function testIsInvalid()
+    {
+        $this->mapper->expects($this->once())
+                     ->method('findByUsername')
+                     ->with('zfcUser')
+                     ->will($this->returnValue(false));
+
+        $result = $this->validator->isValid('zfcUser');
+        $this->assertFalse($result);
+
+        $options = $this->validator->getOptions();
+        $this->assertArrayHasKey(\ZfcUser\Validator\AbstractRecord::ERROR_NO_RECORD_FOUND, $options['messages']);
+        $this->assertEquals($options['messageTemplates'][\ZfcUser\Validator\AbstractRecord::ERROR_NO_RECORD_FOUND], $options['messages'][\ZfcUser\Validator\AbstractRecord::ERROR_NO_RECORD_FOUND]);
     }
 }
