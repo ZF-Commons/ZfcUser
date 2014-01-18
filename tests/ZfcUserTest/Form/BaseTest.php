64de7b549920efac4996ae401dba097f810d6ec7
Daniel Strøm <danielss89@gmail.com>
2014-01-18 23:14:26 +0100

    Added Base form test

--- tests/ZfcUserTest/Form/BaseTest.php
+++ tests/ZfcUserTest/Form/BaseTest.php
@@ -2,14 +2,26 @@
 
 namespace ZfcUserTest\Form;
 
-use ZfcUser\Form\Base as Form;
+use ZfcUserTest\Form\BaseExtension as Form;
 
 class BaseTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @covers ZfcUser\Form\Base::__construct
+     */
     public function testConstruct()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $options = $this->getMock('ZfcUser\Options\RegistrationOptionsInterface');
+        $form = new Form($options);
+
+        $elements = $form->getElements();
+
+        $this->assertArrayHasKey('username', $elements);
+        $this->assertArrayHasKey('email', $elements);
+        $this->assertArrayHasKey('display_name', $elements);
+        $this->assertArrayHasKey('password', $elements);
+        $this->assertArrayHasKey('passwordVerify', $elements);
+        $this->assertArrayHasKey('submit', $elements);
+        $this->assertArrayHasKey('userId', $elements);
     }
 }
