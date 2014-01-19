839c862c4802b92e5d9095f121aa6cd53207b691
Daniel Strøm <danielss89@gmail.com>
2014-01-19 15:37:04 +0100

    Added Register form test

--- tests/ZfcUserTest/Form/RegisterTest.php
+++ tests/ZfcUserTest/Form/RegisterTest.php
@@ -6,27 +6,61 @@ use ZfcUser\Form\Register as Form;
 
 class RegisterTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @covers ZfcUser\Form\Register::__construct
+     */
     public function testConstruct()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $options = $this->getMock('ZfcUser\Options\RegistrationOptionsInterface');
+        $options->expects($this->once())
+            ->method('getEnableUsername')
+            ->will($this->returnValue(false));
+        $options->expects($this->once())
+            ->method('getEnableDisplayName')
+            ->will($this->returnValue(false));
+        $options->expects($this->any())
+            ->method('getUseRegistrationFormCaptcha')
+            ->will($this->returnValue(false));
+        $form = new Form(null, $options);
+
+        $elements = $form->getElements();
+
+        $this->assertArrayNotHasKey('userId', $elements);
+        $this->assertArrayNotHasKey('username', $elements);
+        $this->assertArrayNotHasKey('display_name', $elements);
+        $this->assertArrayHasKey('email', $elements);
+        $this->assertArrayHasKey('password', $elements);
+        $this->assertArrayHasKey('passwordVerify', $elements);
     }
 
-    public function testSetRegistrationOptions()
+    /**
+     * @covers ZfcUser\Form\Register::setRegistrationOptions
+     * @covers ZfcUser\Form\Register::getRegistrationOptions
+     */
+    public function testSetGetRegistrationOptions()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $options = $this->getMock('ZfcUser\Options\RegistrationOptionsInterface');
+        $options->expects($this->once())
+                ->method('getEnableUsername')
+                ->will($this->returnValue(false));
+        $options->expects($this->once())
+                ->method('getEnableDisplayName')
+                ->will($this->returnValue(false));
+        $options->expects($this->any())
+                ->method('getUseRegistrationFormCaptcha')
+                ->will($this->returnValue(false));
+        $form = new Form(null, $options);
+
+        $this->assertSame($options, $form->getRegistrationOptions());
+
+        $optionsNew = $this->getMock('ZfcUser\Options\RegistrationOptionsInterface');
+        $form->setRegistrationOptions($optionsNew);
+        $this->assertSame($optionsNew, $form->getRegistrationOptions());
     }
 
-    public function testGetRegistrationOptions()
-    {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
+    /**
+     * @covers ZfcUser\Form\Register::getRegistrationOptions
+     */
     public function testSetCaptchaElement()
     {
         $this->markTestIncomplete(
