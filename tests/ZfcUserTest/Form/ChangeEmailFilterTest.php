0b6e75be28984ff6e3e8d0048286d5f67f1e6a47
Daniel Strøm <danielss89@gmail.com>
2014-01-19 15:14:30 +0100

    Added ChangeEmailFilter and ChangePasswordFilter tests

--- tests/ZfcUserTest/Form/ChangeEmailFilterTest.php
+++ tests/ZfcUserTest/Form/ChangeEmailFilterTest.php
@@ -2,28 +2,43 @@
 
 namespace ZfcUserTest\Form;
 
-use ZfcUser\Form\ChangeEmailFilter as Form;
+use ZfcUser\Form\ChangeEmailFilter as Filter;
 
 class ChangeEmailFilterTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @covers ZfcUser\Form\ChangeEmailFilter::__construct
+     */
     public function testConstruct()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $options = $this->getMock('ZfcUser\Options\ModuleOptions');
+        $options->expects($this->once())
+                ->method('getAuthIdentityFields')
+                ->will($this->returnValue(array()));
+
+        $validator = $this->getMockBuilder('ZfcUser\Validator\NoRecordExists')->disableOriginalConstructor()->getMock();
+        $filter = new Filter($options, $validator);
+
+        $inputs = $filter->getInputs();
+        $this->assertArrayHasKey('identity', $inputs);
+        $this->assertArrayHasKey('newIdentity', $inputs);
+        $this->assertArrayHasKey('newIdentityVerify', $inputs);
     }
 
-    public function testGetEmailValidator()
+    /**
+     * @covers ZfcUser\Form\ChangeEmailFilter::getEmailValidator
+     * @covers ZfcUser\Form\ChangeEmailFilter::setEmailValidator
+     */
+    public function testSetGetEmailValidator()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+        $options = $this->getMock('ZfcUser\Options\ModuleOptions');
+        $validatorInit = $this->getMockBuilder('ZfcUser\Validator\NoRecordExists')->disableOriginalConstructor()->getMock();
+        $validatorNew = $this->getMockBuilder('ZfcUser\Validator\NoRecordExists')->disableOriginalConstructor()->getMock();
 
-    public function testSetEmailValidator()
-    {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $filter = new Filter($options, $validatorInit);
+
+        $this->assertSame($validatorInit, $filter->getEmailValidator());
+        $filter->setEmailValidator($validatorNew);
+        $this->assertSame($validatorNew, $filter->getEmailValidator());
     }
 }
--- tests/ZfcUserTest/Form/ChangePasswordFilterTest.php
+++ tests/ZfcUserTest/Form/ChangePasswordFilterTest.php
@@ -2,14 +2,26 @@
 
 namespace ZfcUserTest\Form;
 
-use ZfcUser\Form\ChangePasswordFilter as Form;
+use ZfcUser\Form\ChangePasswordFilter as Filter;
 
 class ChangePasswordFilterTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @covers ZfcUser\Form\ChangePasswordFilter::__construct
+     */
     public function testConstruct()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $options = $this->getMock('ZfcUser\Options\ModuleOptions');
+        $options->expects($this->once())
+            ->method('getAuthIdentityFields')
+            ->will($this->returnValue(array()));
+
+        $filter = new Filter($options);
+
+        $inputs = $filter->getInputs();
+        $this->assertArrayHasKey('identity', $inputs);
+        $this->assertArrayHasKey('credential', $inputs);
+        $this->assertArrayHasKey('newCredential', $inputs);
+        $this->assertArrayHasKey('newCredentialVerify', $inputs);
     }
 }
