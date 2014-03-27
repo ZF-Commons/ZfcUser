e08a482d46908be2760019b74e0fdcc75e452e82
Stijn <Stijn@192.168.0.191>
2014-03-27 21:44:40 +0100

    Changes so that the translator can be set in the global.php autoload

--- config/module.config.php
+++ config/module.config.php
@@ -3,7 +3,9 @@ return array(
     'translator' => array(
         'translation_file_patterns' => array(
             array(
-                'base_dir' => __DIR__ . '/../../language',
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
             ),
         ),
     ),
