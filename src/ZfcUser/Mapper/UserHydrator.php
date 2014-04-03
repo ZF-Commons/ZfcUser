f9b5bb7af3cf2b3f060bb09a4cc7c0ae5be0e7ed
sing88 <carlossing@gmail.com>
2014-04-03 12:29:55 -0500

    Fix line {

--- src/ZfcUser/Mapper/UserHydrator.php
+++ src/ZfcUser/Mapper/UserHydrator.php
@@ -5,7 +5,8 @@ namespace ZfcUser\Mapper;
 use Zend\Stdlib\Hydrator\ClassMethods;
 use ZfcUser\Entity\UserInterface as UserEntityInterface;
 
-class UserHydrator extends ClassMethods {
+class UserHydrator extends ClassMethods 
+{
 
     /**
      * Extract values from an object
@@ -14,7 +15,8 @@ class UserHydrator extends ClassMethods {
      * @return array
      * @throws Exception\InvalidArgumentException
      */
-    public function extract($object) {
+    public function extract($object) 
+    {
         if (!$object instanceof UserEntityInterface) {
             throw new Exception\InvalidArgumentException('$object must be an instance of ZfcUser\Entity\UserInterface');
         }
@@ -36,7 +38,8 @@ class UserHydrator extends ClassMethods {
      * @return UserInterface
      * @throws Exception\InvalidArgumentException
      */
-    public function hydrate(array $data, $object) {
+    public function hydrate(array $data, $object) 
+    {
         if (!$object instanceof UserEntityInterface) {
             throw new Exception\InvalidArgumentException('$object must be an instance of ZfcUser\Entity\UserInterface');
         }
@@ -44,7 +47,8 @@ class UserHydrator extends ClassMethods {
         return parent::hydrate($data, $object);
     }
 
-    protected function mapField($keyFrom, $keyTo, array $array) {
+    protected function mapField($keyFrom, $keyTo, array $array) 
+    {
         $array[$keyTo] = $array[$keyFrom];
         unset($array[$keyFrom]);
         return $array;
