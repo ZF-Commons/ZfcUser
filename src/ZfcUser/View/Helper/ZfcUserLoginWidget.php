ea3a64f2c01551414884bfe1170efffc65aa3886
Mike Kelly <michael.kelly806@gmail.com>
2014-04-19 18:21:45 +0100

    Fixed build errors

--- src/ZfcUser/View/Helper/ZfcUserLoginWidget.php
+++ src/ZfcUser/View/Helper/ZfcUserLoginWidget.php
@@ -48,7 +48,7 @@ class ZfcUserLoginWidget extends AbstractHelper
         $vm = new ViewModel(array(
             'loginForm' => $this->getLoginForm(),
             'redirect'  => $redirect,
-            'enableRegistration' => $this->registrationOptions->getEnableRegistration()
+            'enableRegistration' => $this->enableRegistration,
         ));
         $vm->setTemplate($this->viewTemplate);
         if ($render) {
