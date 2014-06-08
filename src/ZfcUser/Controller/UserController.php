f5d50dddd6640f613e9452d043be0000cfde5dcd
Piotr Zając <piotr.zajac@unixslayer.pl>
2014-06-08 21:20:20 +0200

    removed TODO comment

--- src/ZfcUser/Controller/UserController.php
+++ src/ZfcUser/Controller/UserController.php
@@ -232,7 +232,6 @@ class UserController extends AbstractActionController
             return $this->forward()->dispatch(static::CONTROLLER_NAME, array('action' => 'authenticate'));
         }
 
-        // TODO: Add the redirect parameter here...
         return $this->redirect()->toUrl($this->url()->fromRoute($this->getOptions()->getRegisterRedirectRoute()) . ($redirect ? '?redirect='. rawurlencode($redirect) : ''));
     }
 
