2c900aaecbc831735408a92d1573aa2418f11631
1CASH1 <cash_dip@hotmail.com>
2014-06-16 19:54:09 -0500

    Update schema.sql

--- data/schema.sql
+++ data/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE user
 (
-    user_id       INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    user_id       int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     username      VARCHAR(255) DEFAULT NULL UNIQUE,
     email         VARCHAR(255) DEFAULT NULL UNIQUE,
     display_name  VARCHAR(50) DEFAULT NULL,
     password      VARCHAR(128) NOT NULL,
     state         SMALLINT
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
