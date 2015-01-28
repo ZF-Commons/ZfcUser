d9b27aabf27335ced1f15c1d9f85fd13bece4d47
Clayton Daley <clayton.daley@gmail.com>
2015-01-28 16:22:54 -0500

    - move registration dependencies from Base constructor to Registration constructor

--- src/ZfcUser/Form/Register.php
+++ src/ZfcUser/Form/Register.php
@@ -23,6 +23,17 @@ class Register extends Base
         $this->setRegistrationOptions($options);
         parent::__construct($name);
 
+        if ($this->getRegistrationOptions()->getUseRegistrationFormCaptcha()) {
+            $this->add(array(
+                'name' => 'captcha',
+                'type' => 'Zend\Form\Element\Captcha',
+                'options' => array(
+                    'label' => 'Please type the following text',
+                    'captcha' => $this->getRegistrationOptions()->getFormCaptchaOptions(),
+                ),
+            ));
+        }
+
         $this->remove('userId');
         if (!$this->getRegistrationOptions()->getEnableUsername()) {
             $this->remove('username');
